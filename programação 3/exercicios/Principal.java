d3237c32cb98cfe3b29bbe5b329f862a0777f6e5
Lucas <souzalucas6@hotmail.com>
2024-09-18 11:31:12 -0300

    Removendo arquivos desnecessários e subindo ex01 prog3

---
+++ programação 3/exercicios/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+        Circulo novoCirculo = new Circulo(3);
+        System.out.println(novoCirculo.calcularArea);
+        System.out.println(novoCirculo.calcularPerimetro);
+    }
+}
---
+++ programação 3/exercicios/ex01.java
@@ -0,0 +1,13 @@
+//Crie uma classe chamada “Círculo” que possua um atributo para armazenar o raio e métodos para calcular a área e o perímetro do círculo.
+
+public class Circulo{
+    private float raio;
+
+    public void calcularArea(){
+        return 2*3,14
+    }
+
+    public void calcularPerimetro(){
+
+    }
+}
